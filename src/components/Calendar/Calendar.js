188eb736fbf3c6b3b6be4670c0691fe6830881fd
andrew macbook <andrewCheng078@gmail.com>
2019-07-14 20:18:41 +0800

    add moment

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,107 +1,175 @@
 import React, { Component } from 'react'
 import './Calendar.scss';
-const moment = require('moment');
-// import jsonData from '../../data/data1.json';
-// console.log('jsonData',jsonData);
-console.log(moment().format())
+import moment from 'moment';
+
+
 
 export default class Calendar extends Component {
-constructor() {
-            super();
-        this.state = {
-            initYearMonth:'',
 
-        }
+    state = {
+        initYearMonth: moment().format("YYYYMM") || this.props.initYearMonth,
+        firstDayWeek: '',
+        endDayWeek: '',
+        daysInMonth: '',
+        days:[],
+        data:[],
+        listLeft: '',
+        listCenter: '',
+        listRight: '',
+
+    }
+
+    filterData = () => {
+        //do something
+
+    } 
+
+    createDays = () => {
+        const { firstDayWeek, endDayWeek, daysInMonth } = this.state;
+
+    }
+
+     prevMonth = async ()=> {
+
+        let prev = moment(this.state.initYearMonth, "YYYYMM").add(-1, "M").format("YYYYMM");
+        await this.setState({
+            initYearMonth: prev,
+
+        })
+        await this.listValue();
+        console.log('prev',prev);
+    }
+
+    nextMonth = async () => {
+
+        let next = moment(this.state.initYearMonth, "YYYYMM").add(+1, "M").format("YYYYMM");
+        await this.setState({
+            initYearMonth: next,
+
+        })
+        await this.listValue();
+        console.log('next',next);
+    }
+
+    leftList = () => {this.prevMonth()}
+    centerList = () => { /*center*/}
+    rightList = () => {this.nextMonth()}
+
+    listValue = () => {
+        this.setState({
+            listLeft: moment(this.state.initYearMonth, "YYYYMM").add(-1, "M").format("YYYY M"),
+            listCenter: this.state.initYearMonth,
+            listRight: moment(this.state.initYearMonth, "YYYYMM").add(+1, "M").format("YYYY M"),
+        })
+    }
+
+
+    componentDidMount() {
+        let { initYearMonth } = this.state;
+        this.listValue();
+        const firstDayWeek = moment(initYearMonth, "YYYYMM").startOf('month').format('d');
+        const endDayWeek = moment(initYearMonth, "YYYYMM").endOf('month').format('d');
+        const daysInMonth = moment(initYearMonth).daysInMonth();
+        console.log('firstDayWeek', firstDayWeek);
+        console.log('endDayWeek', endDayWeek);
+        console.log('daysInMonth', daysInMonth);
+        this.setState({
+            firstDayWeek: firstDayWeek,
+            endDayWeek: endDayWeek,
+            daysInMonth: daysInMonth,
+        });
     }
 
 
     render() {
+        const { initYearMonth, listLeft, listCenter, listRight, } = this.state;
         return (
             <div>
-                 {/* 可加上這兩個修飾符來切換日曆模式或列表模式 calendars_daymode,calendars_listmode */}
+                {/* 可加上這兩個修飾符來切換日曆模式或列表模式 calendars_daymode,calendars_listmode */}
                 <div className="calendars calendars_daymode">
+                <a href="#" className="prev on"  style={{float:'right'}}>切換模式</a>
                     <div className="calendars_tabWrap">
-                        <a href="#" className="prev on">向左</a>
+                        <a href="#" className="prev on" onClick={this.prevMonth}>向左</a>
                         <ul className="ntb_tab">
-                            <li className="tab">
-                                <a href="#"><span>2017 7月</span></a>
+                            <li className="tab" onClick={this.leftList}>
+                                <a href="#"><span>{listLeft}月</span></a>
                             </li>
-                            <li className="tab">
-                                <a href="#"><span>2017 8月</span></a>
+                            <li className="tab" onClick={this.centerList}>
+                                <a href="#"><span>{listCenter}月</span></a>
                             </li>
-                            <li className="tab">
-                                <a href="#"><span>2017 9月</span></a>
+                            <li className="tab" onClick={this.rightList}>
+                                <a href="#"><span>{listRight}月</span></a>
                             </li>
                         </ul>
-                        <a href="#" className="next on">向右</a>
+                        <a href="#" className="next on" onClick={this.nextMonth}>向右</a>
                     </div>
                     <table>
                         <thead>
-                                <tr className="calendars_weeksWrap">
-                                    <th>星期日</th>
-                                    <th>星期一</th>
-                                    <th>星期二</th>
-                                    <th>星期三</th>
-                                    <th>星期四</th>
-                                    <th>星期五</th>
-                                    <th>星期六</th>
-                                </tr>
+                            <tr className="calendars_weeksWrap">
+                                <th>星期日</th>
+                                <th>星期一</th>
+                                <th>星期二</th>
+                                <th>星期三</th>
+                                <th>星期四</th>
+                                <th>星期五</th>
+                                <th>星期六</th>
+                            </tr>
                         </thead>
                         <tbody>
-                           <tr>
-                               <td>1</td>
-                               <td>2</td>
-                               <td>3</td>
-                               <td>4</td>
-                               <td>5</td>
-                               <td>6</td>
-                               <td>7</td>
-                           </tr>
-                           <tr>
-                               <td>1</td>
-                               <td>2</td>
-                               <td>3</td>
-                               <td>4</td>
-                               <td>5</td>
-                               <td>6</td>
-                               <td>7</td>
-                           </tr>
-                           <tr>
-                               <td>1</td>
-                               <td>2</td>
-                               <td>3</td>
-                               <td>4</td>
-                               <td>5</td>
-                               <td>6</td>
-                               <td>7</td>
-                           </tr>
-                           <tr>
-                               <td>1</td>
-                               <td>2</td>
-                               <td>3</td>
-                               <td>4</td>
-                               <td>5</td>
-                               <td>6</td>
-                               <td>7</td>
-                           </tr>
-                           <tr>
-                               <td>1</td>
-                               <td>2</td>
-                               <td>3</td>
-                               <td>4</td>
-                               <td>5</td>
-                               <td>6</td>
-                               <td>7</td>
-                           </tr>
-                           <tr>
-                               <td>1</td>
-                               <td>2</td>
-                               <td>3</td>
-                               <td>4</td>
-                               <td>5</td>
-                               <td>6</td>
-                               <td>7</td>
-                           </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
@@ -142,3 +210,14 @@ constructor() {
 
 
 
+                    // <td className="currentDays" date="2019/08/01">
+                    // <div className="day js_day">
+                    //     <span class="num">1</span>
+                    //     <span class="tip js_tip">成團</span>
+                    //     <div className="details js_details">
+                    //         <span class="status js_status lb-org">報名</span>
+                    //         <span class="sell">可賣 : <i class="js_sell">2</i></span>
+                    //         <span class="group">團位 : <i class="js_group">8</i></span>
+                    //         <span class="price js_price">$22,900</span>
+                    //     </div>
+                    // </div> 
